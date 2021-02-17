48fe7df1a27ac8cd394cf7a06a83ff2583e9516f
Ayem Kpenkaan <ayemkpenkaan@gmail.com>
2021-02-17 16:18:48 -0500

    Changing db

--- events.py
+++ events.py
@@ -6,7 +6,7 @@ from flask import Flask, request, session, url_for, redirect, render_template, a
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from models import db, User, Event
+#from models import db, User, Event
 
 app = Flask(__name__)
 
@@ -17,12 +17,56 @@ SECRET_KEY = 'development key'
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(app.root_path, 'events.db')
 
-app.config.from_object(__name__)
-app.config.from_envvar('EVENTS_SETTINGS', silent=True)
+#app.config.from_object(__name__)
+#app.config.from_envvar('EVENTS_SETTINGS', silent=True)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #silence deprecation warning
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://akacuggfuggebg:b3bec0e1d92f2a7e973f1fe974838d349655a605b71ffdb66a6ae732ae394a13@ec2-34-192-58-41.compute-1.amazonaws.com:5432/doairivml5hh9'
 
 db = SQLAlchemy(app)
+
+
+class User(db.Model): 
+    user_id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(24), nullable=False)
+    pw_hashed = db.Column(db.String(64), nullable=False)
+
+    #1 to many with host and events
+    #many to many with attendees and events
+
+    def __init__(self,username,pw_hashed):
+        self.username = username
+        self.pw_hashed = pw_hashed
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+#association table for events and attendees
+attendees = db.Table('attendees',
+    db.Column('attendee_id',db.Integer,db.ForeignKey('user.user_id')),
+    db.Column('event_id',db.Integer,db.ForeignKey('event.event_id'))
+)
+
+#event has host id which we use to check if we can delete event
+#to check users in event we use attendees relationship
+class Event(db.Model):
+    event_id = db.Column(db.Integer,primary_key=True)
+    host_id = db.Column(db.Integer, db.ForeignKey('user.user_id'),nullable=False)
+    title = db.Column(db.Text,nullable=False)
+    description = db.Column(db.Text,nullable=True)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    attendees = db.relationship('User',secondary=attendees,backref=db.backref('event',lazy='dynamic'),lazy='dynamic')
+
+    def __init__(self,host_id,title,description,start_date,end_date):
+        self.host_id = host_id
+        self.title = title
+        self.description = description
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def __repr__(self):
+            return '<Event {}>'.format(self.event_id)
     
 
 def get_userId(username):
